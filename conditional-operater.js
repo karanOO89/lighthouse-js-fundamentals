ec1cdb0ba1d30f3e3a205895444266672def217d
karanOO89 <gk.987680@gmail.com>
2021-03-14 18:48:24 +0000

    Add more conditional examples

---
+++ conditional-operater.js
@@ -0,0 +1,17 @@
+const isCitizen = true;
+const age = 26;
+let temperature=55;
+let raining=false; 
+/*if (isCitizen && age > 18) {
+  console.log("You are eligible to vote.");
+}
+*/
+if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea…");
+}
+
+if (!raining) {
+  console.log("Leave your umbrella at home!");
+}
+
+
