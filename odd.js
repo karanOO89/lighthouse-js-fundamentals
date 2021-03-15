43874d269464039e0e7fec2bf3d8647f731c26d0
karanOO89 <gk.987680@gmail.com>
2021-03-15 03:10:35 +0000

    isOdd function is added

---
+++ odd.js
@@ -0,0 +1,8 @@
+const isOdd  = function (num) {
+    return num % 2 !== 0;
+    }
+//   const tenIsEven = isEven(10);
+// const elevenIsEven = isEven(11);
+
+console.log("number 3 is odd: " + isOdd(3));
+ console.log("number 8 is odd: " + isOdd(8));
